86f88127800f3a7a3cf2e221b7c6635c02c2b20b
Thano0s <himanshupatil45h@gmail.com>
2024-01-11 16:52:42 +0530

    made changes in Phone auth and Authentication. Turn on noHistory of Authentication activity

--- app/src/main/java/com/application/parkpilot/Authentication.kt
+++ app/src/main/java/com/application/parkpilot/Authentication.kt
@@ -6,6 +6,7 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.ProgressBar
 import android.widget.ScrollView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import com.chaos.view.PinView
@@ -18,30 +19,56 @@ class Authentication: AppCompatActivity() {
         val buttonVerifyPhoneNumber: Button = findViewById(R.id.buttonVerifyPhoneNumber)
         val progressBar: ProgressBar = findViewById(R.id.progressBar)
         val phoneAuth = PhoneAuthActivity(this)
+        val loginScrollView: ScrollView = findViewById(R.id.loginScrollView)
+        val OTPScrollView:ScrollView = findViewById(R.id.OTPScrollView)
+
+        loginScrollView.visibility = View.VISIBLE
+        OTPScrollView.visibility = View.GONE
 
         buttonVerifyPhoneNumber.setOnClickListener {
             progressBar.visibility = View.VISIBLE
             val countryCodePicker:com.hbb20.CountryCodePicker = findViewById(R.id.countryCodePicker)
-            phoneAuth.startPhoneNumberVerification(countryCodePicker.selectedCountryCodeWithPlus + editTextPhoneNumber.text.toString())
+            val numberTextView: TextView = findViewById(R.id.numberTextView)
+
+            val phoneNumber:String = countryCodePicker.selectedCountryCodeWithPlus + editTextPhoneNumber.text.toString()
+            numberTextView.text = phoneNumber
+            phoneAuth.startPhoneNumberVerification(phoneNumber)
         }
 
         val OTPPinView:PinView = findViewById(R.id.OTPPinView)
 
-        phoneAuth.storedVerificationId.observe(this, Observer {
+        phoneAuth.storedVerificationId.observe(this) {
             //Do something with the changed value -> it
             progressBar.visibility = View.GONE
 
-            val loginScrollView: ScrollView = findViewById(R.id.loginScrollView)
-            val OTPScrollView:ScrollView = findViewById(R.id.OTPScrollView)
+            // if OTP send successfully
+            if (phoneAuth.storedVerificationId.value != null) {
+                loginScrollView.visibility = View.GONE
+                OTPScrollView.visibility = View.VISIBLE
+                OTPPinView.text = null
+            }
+            // if OTP not send (error)
+            else {
 
-            loginScrollView.visibility = View.GONE
-            OTPScrollView.visibility = View.VISIBLE
-            OTPPinView.text = null
-        })
-
-        val OTPVerivicationButton:Button = findViewById(R.id.OTPVerificationButton)
-        OTPVerivicationButton.setOnClickListener {
-           phoneAuth.verifyPhoneNumberWithCode(phoneAuth.storedVerificationId.value,OTPPinView.toString())
+            }
         }
+
+        val OTPVerificationButton:Button = findViewById(R.id.OTPVerificationButton)
+        OTPVerificationButton.setOnClickListener {
+           phoneAuth.verifyPhoneNumberWithCode(phoneAuth.storedVerificationId.value,OTPPinView.text.toString())
+        }
+
+        phoneAuth.storedTaskResult.observe(this){
+
+            // Credential match (OTP is correct)
+            if(phoneAuth.storedTaskResult.value != null){
+
+            }
+            else{
+                OTPPinView.visibility = View.GONE
+                loginScrollView.visibility = View.VISIBLE
+            }
+        }
+
     }
 }
