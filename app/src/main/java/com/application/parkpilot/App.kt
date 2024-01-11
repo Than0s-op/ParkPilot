677c3390d3917741ea428fecd9213d297bf1258b
Thano0s <himanshupatil45h@gmail.com>
2024-01-11 15:09:50 +0530

    theme and color

--- app/src/main/java/com/application/parkpilot/App.kt
+++ app/src/main/java/com/application/parkpilot/App.kt
@@ -1,8 +1,9 @@
 package com.application.parkpilot
+
 import android.app.Application
 import com.google.android.material.color.DynamicColors
 
-class App: Application() {
+class App : Application() {
     override fun onCreate() {
         super.onCreate()
         DynamicColors.applyToActivitiesIfAvailable(this)
