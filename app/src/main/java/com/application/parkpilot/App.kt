1ed0122770be6a119717e335fa82c44cc3f1e091
Thano0s <himanshupatil45h@gmail.com>
2024-01-11 14:55:11 +0530

    dynamic color

---
+++ app/src/main/java/com/application/parkpilot/App.kt
@@ -0,0 +1,10 @@
+package com.application.parkpilot
+import android.app.Application
+import com.google.android.material.color.DynamicColors
+
+class App: Application() {
+    override fun onCreate() {
+        super.onCreate()
+        DynamicColors.applyToActivitiesIfAvailable(this)
+    }
+}
