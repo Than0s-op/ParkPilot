40fa534c09608fe27b30275e58acf91b964cef25
Thano0s <himanshupatil45h@gmail.com>
2024-01-20 16:06:49 +0530

    integrate map_pins with single top behaviour

---
+++ app/src/main/java/com/application/parkpilot/BlankFragment.kt
@@ -0,0 +1,11 @@
+package com.application.parkpilot
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class BlankFragment : Fragment(R.layout.fragment_blank) {
+
+}
--- app/src/main/java/com/application/parkpilot/OSM.kt
+++ app/src/main/java/com/application/parkpilot/OSM.kt
@@ -1,16 +1,19 @@
 package com.application.parkpilot
 
-import android.R
 import android.location.Address
 import android.location.Geocoder
 import android.location.Location
-import android.view.MotionEvent
-import android.widget.Toast
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import kotlinx.coroutines.tasks.await
+import org.osmdroid.config.Configuration
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory
 import org.osmdroid.util.GeoPoint
 import org.osmdroid.views.MapView
@@ -20,16 +23,18 @@ import org.osmdroid.views.overlay.ItemizedOverlay
 import org.osmdroid.views.overlay.OverlayItem
 
 
-class OSM<Act : AppCompatActivity>(private val mapView: MapView, private val obj: Act) {
+class OSM<Act : AppCompatActivity>(private val mapView: MapView, private val activity: Act) {
 
     // object creation of geocoder
-    private val geocoder: Geocoder = Geocoder(obj)
+    private val geocoder: Geocoder = Geocoder(activity)
 
     // object creation of fusedLocation
     private val fusedLocationClient: FusedLocationProviderClient =
-        LocationServices.getFusedLocationProviderClient(obj)
+        LocationServices.getFusedLocationProviderClient(activity)
 
     init {
+        // I don't know
+        Configuration.getInstance().userAgentValue = "ParkPilot"
 
         // mapView default settings
         mapView.setTileSource(TileSourceFactory.MAPNIK)
@@ -68,65 +73,79 @@ class OSM<Act : AppCompatActivity>(private val mapView: MapView, private val obj
 
     fun search(searchQuery: String): Address? {
         // this is deprecated in API 33
-        val result = geocoder.getFromLocationName(searchQuery,1)
-        if(result != null && result.size != 0){
-            return result.first()
+        try {
+            // It can throw exception
+            val result = geocoder.getFromLocationName(searchQuery, 1)
+
+            // If result has result then...
+            if (result != null && result.size != 0) {
+                return result.first()
+            }
         }
+        catch(e:Exception){
+            println("Exception:OSM:search ${e.message}")
+        }
+        //
         return null
     }
 
-    suspend fun getLastKnowLocation():GeoPoint? {
+    suspend fun getLastKnowLocation(): GeoPoint? {
 
         // permission class object creation
-        val obj = PermissionRequest(obj)
+        val activity = PermissionRequest(activity)
 
         // current location set to null (if permission is not granted, we can't enter in "if" block)
-        var currentLocation:GeoPoint? = null
+        var currentLocation: GeoPoint? = null
 
         // check is location permission granted or not. If Not request to user for location permission
-        if (obj.locationPermissionRequest()) {
+        if (activity.locationPermissionRequest()) {
 
             // request to turn on location(GPS)
-            obj.GPSPermissionRequest()
+            activity.GPSPermissionRequest()
 
             // below code will give last know location of user. But It will "null" if user's GPS is turned off
             fusedLocationClient.lastLocation
                 .addOnSuccessListener { location: Location? ->
                     // Got last known location. In some rare situations this can be null.
-                    if(location != null){
-                        currentLocation = GeoPoint(location.latitude,location.longitude)
+                    if (location != null) {
+                        currentLocation = GeoPoint(location.latitude, location.longitude)
                     }
                 }.await()
         }
         return currentLocation
     }
 
-    fun setMarkerOnPosition(geoPoint:GeoPoint) {
+    fun setPinsOnPosition(geoPoints:ArrayList<ParkPilotMapPin>, singleTapTask:(String)-> Unit) {
 
-        val overlayItem = OverlayItem("San Fransisco", "California", geoPoint)
-
-        val markerDrawable = ContextCompat.getDrawable(obj, com.application.parkpilot.R.drawable.park_pilot_map_marker)
-        overlayItem.setMarker(markerDrawable)
-
+        // contain all pins with "single tap" and "long press" binding
         val overlayItemArrayList = ArrayList<OverlayItem>()
-        overlayItemArrayList.add(overlayItem)
+
+        // create pin image
+        val markerDrawable = ContextCompat.getDrawable(activity, R.drawable.park_pilot_map_marker)
+
+        for(geoPoint in geoPoints){
+            val overlayItem = OverlayItem(geoPoint.title, geoPoint.UID, geoPoint.coordinates)
+            overlayItem.setMarker(markerDrawable)
+            overlayItemArrayList.add(overlayItem)
+        }
+
+        // binding "single tap" and "long press" event to all overlay Items
         val locationOverlay: ItemizedOverlay<OverlayItem> = ItemizedIconOverlay(
             overlayItemArrayList,
             object : OnItemGestureListener<OverlayItem> {
                 override fun onItemSingleTapUp(i: Int, overlayItem: OverlayItem): Boolean {
-                    println("Single Tap Up on maker")
+                    singleTapTask(overlayItem.snippet)
                     return true // Handled this event.
                 }
 
                 override fun onItemLongPress(i: Int, overlayItem: OverlayItem): Boolean {
-                    println("Long press on marker")
                     return false
                 }
             },
-            obj
+            activity
         )
 
-        this.mapView.overlays.add(locationOverlay)
+        // update overlays(pins) on mapView
         mapView.overlays.add(locationOverlay)
     }
 }
