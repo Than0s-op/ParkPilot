f35be991ea9de64f92383a2022045f0e9b460f77
Thano0s <himanshupatil45h@gmail.com>
2024-01-22 10:53:36 +0530

    Date picker module

--- app/src/main/java/com/application/parkpilot/UserRegisterActivity.kt
+++ app/src/main/java/com/application/parkpilot/UserRegisterActivity.kt
@@ -1,13 +1,25 @@
 package com.application.parkpilot
 
+import android.icu.util.Calendar
+import android.icu.util.TimeZone
 import android.os.Bundle
-import android.os.PersistableBundle
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import java.text.ParseException
+import java.text.SimpleDateFormat
+import java.time.LocalDate
+import java.util.Date
 
-class UserRegisterActivity: AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+class UserRegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
         setContentView(R.layout.user_register)
 
+        val editTextBirthDate: EditText = findViewById(R.id.editTextBirthDate)
+        editTextBirthDate.setOnClickListener {
+            val datePicker = DatePicker(this)
+            // date format should be yyyy-mm-dd
+            datePicker.showDatePicker("Select Birth Date","2020-11-20","2021-11-18")
+        }
     }
 }
