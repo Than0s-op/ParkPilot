008fc69c8adbc6950bd19a9976a303ad85079a01
Thano0s <himanshupatil45h@gmail.com>
2024-01-23 08:03:12 +0530

    formating

--- app/src/main/java/com/application/parkpilot/activity/MainActivity.kt
+++ app/src/main/java/com/application/parkpilot/activity/MainActivity.kt
@@ -1,9 +1,9 @@
 package com.application.parkpilot.activity
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.application.parkpilot.activity.HomeActivity
+import androidx.appcompat.app.AppCompatActivity
+import com.application.parkpilot.CompanionObjects
 import com.google.firebase.Firebase
 import com.google.firebase.auth.auth
 
@@ -11,22 +11,30 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val user = Firebase.auth.currentUser
-        if (user != null) {
-            // User is signed in
+        // getting current user status and set to the companion object "currentUser"
+        CompanionObjects.currentUser = Firebase.auth.currentUser
+
+        // User is signed in
+        if (CompanionObjects.currentUser != null) {
+
+            // if user is signIn but not filled the registration information
+            // do something
+
+
+            // creating the home activity intent
             val intent = Intent(this, HomeActivity::class.java)
 
             // clear the activity stack
             intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
 
-            intent.putExtra("result",user)
             startActivity(intent)
-        } else {
-            // No user is signed in
+        }
+        // No user is signed in
+        else {
+            // through user to the authentication activity
             startActivity(Intent(this, AuthenticationActivity::class.java).apply {
                 flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
             })
-//            startActivity(Intent(this,PermissionRequest::class.java))
         }
     }
 }
