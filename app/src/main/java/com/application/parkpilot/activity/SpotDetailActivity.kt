0038c630ffbf0357708e2ef968d0f78e7220a144
Thano0s <himanshupatil45h@gmail.com>
2024-02-09 17:49:40 +0530

    razorpay integrate

--- app/src/main/java/com/application/parkpilot/activity/SpotDetailActivity.kt
+++ app/src/main/java/com/application/parkpilot/activity/SpotDetailActivity.kt
@@ -1,15 +1,18 @@
 package com.application.parkpilot.activity
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.application.parkpilot.R
 import com.application.parkpilot.adapter.CarouselRecyclerView
 import com.application.parkpilot.module.RazorPay
 import com.google.android.material.carousel.CarouselLayoutManager
+import com.razorpay.PaymentResultListener
 
-class SpotDetailActivity: AppCompatActivity(R.layout.spot_detail) {
+class SpotDetailActivity: AppCompatActivity(R.layout.spot_detail), PaymentResultListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val recycleView: RecyclerView = findViewById(R.id.recycleView)
@@ -27,8 +30,17 @@ class SpotDetailActivity: AppCompatActivity(R.layout.spot_detail) {
         recycleView.adapter = CarouselRecyclerView(this,R.layout.square_carousel,arr)
 
         buttonBookNow.setOnClickListener {
-//            val razorpay = RazorPay(this)
-//            razorpay.makePayment("INR",10,"123")
+            // this is temporary
+            val razorpay = RazorPay(this)
+            razorpay.makePayment("INR",10,"123")
         }
+    }
+
+    override fun onPaymentSuccess(razorpayPaymentID: String) {
+        Toast.makeText(this,"Payment successfully completed $razorpayPaymentID", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onPaymentError(code: Int, response: String) {
+        Toast.makeText(this,"Payment failed $response",Toast.LENGTH_SHORT).show()
     }
 }
--- app/src/main/java/com/application/parkpilot/module/RazorPay.kt
+++ app/src/main/java/com/application/parkpilot/module/RazorPay.kt
@@ -11,21 +11,20 @@ import com.razorpay.Checkout
 import com.razorpay.PayloadHelper
 import com.razorpay.PaymentResultListener
 
-class RazorPay:Activity(), PaymentResultListener {
-    private lateinit var checkout:Checkout
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        Checkout.preload(this);
-        Checkout.sdkCheckIntegration(this)
+class RazorPay(private val activity:Activity){
+    private var checkout:Checkout
+    init{
+        Checkout.preload(activity)
+        Checkout.sdkCheckIntegration(activity)
 
         // checkout initializing
         checkout = Checkout()
 
         // it will set razor api key
-        checkout.setKeyID(this.getString(R.string.razor_pay_test_key))
+        checkout.setKeyID(activity.getString(R.string.razor_pay_test_key))
 
         // it will set image to gateway
-        // checkout.setImage()
+        checkout.setImage(R.drawable.heavy_vehicle)
     }
 
     fun makePayment(currency:String, amount:Int, orderId:String){
@@ -34,24 +33,6 @@ class RazorPay:Activity(), PaymentResultListener {
         // currency = "INR"... like this
         val payloadHelper = PayloadHelper(currency,amount,orderId)
         payloadHelper.name = "Than0s"
-        checkout.open(this,payloadHelper.getJson())
-    }
-
-    override fun onPaymentError(errorCode: Int, response: String?) {
-        /**
-         * Add your logic here for a failed payment response
-         */
-        println("razor failed: $response")
-        Toast.makeText(this, "Payment Failed due to error : $response", Toast.LENGTH_SHORT).show()
-        QRGenerator(this).generate(Firebase.auth.currentUser!!.uid)
-    }
-
-    override fun onPaymentSuccess(razorpayPaymentId: String?) {
-        /**
-         * Add your logic here for a successful payment response
-         */
-        println("razor success: $razorpayPaymentId")
-        Toast.makeText(this, "Payment is successful : $razorpayPaymentId", Toast.LENGTH_SHORT).show()
-        QRGenerator(this).generate(Firebase.auth.currentUser!!.uid)
+        checkout.open(activity,payloadHelper.getJson())
     }
 }
