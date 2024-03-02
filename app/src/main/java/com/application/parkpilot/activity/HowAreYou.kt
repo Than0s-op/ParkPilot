cf905c29df125d98a09bf95964bd775238ddef8b
Thano0s <himanshupatil45h@gmail.com>
2024-03-02 11:02:18 +0530

    how are you activity created

--- app/src/main/java/com/application/parkpilot/activity/HowAreYou.kt
+++ app/src/main/java/com/application/parkpilot/activity/HowAreYou.kt
@@ -6,18 +6,28 @@ import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.application.parkpilot.R
 
-class HowAreYou: AppCompatActivity(R.layout.how_are_you) {
+
+// NOTE: this activity will start, when user would login first time
+class HowAreYou : AppCompatActivity(R.layout.how_are_you) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val buttonOwner:Button = findViewById(R.id.buttonOwner)
+
+        // owner button creating
+        val buttonOwner: Button = findViewById(R.id.buttonOwner)
+
+        // finder button creating
         val buttonFinder: Button = findViewById(R.id.buttonFinder)
 
+        // when owner button clicked
         buttonOwner.setOnClickListener {
-            startActivity(Intent(this,ParkRegisterActivity::class.java))
+            // start owner registration activity
+            startActivity(Intent(this, ParkRegisterActivity::class.java))
         }
 
-        buttonFinder.setOnClickListener{
-            startActivity(Intent(this,UserRegisterActivity::class.java))
+        // when finder button clicked
+        buttonFinder.setOnClickListener {
+            // start finder registration activity
+            startActivity(Intent(this, UserRegisterActivity::class.java))
         }
     }
 }
