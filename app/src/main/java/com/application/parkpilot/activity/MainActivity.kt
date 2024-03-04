ecb6e615c62c1438bc2238559d98510381fcf5a7
Thano0s <himanshupatil45h@gmail.com>
2024-03-04 11:14:34 +0530

    adapte main activity for 3 type of user

--- app/src/main/java/com/application/parkpilot/activity/MainActivity.kt
+++ app/src/main/java/com/application/parkpilot/activity/MainActivity.kt
@@ -3,7 +3,9 @@ package com.application.parkpilot.activity
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
-import com.application.parkpilot.module.firebase.database.User as fireStoreUser
+import com.application.parkpilot.User
+import com.application.parkpilot.module.firebase.database.StationLocation as FireStoreStationLocation
+import com.application.parkpilot.module.firebase.database.User as FireStoreUser
 import com.application.parkpilot.User as appUser
 import com.google.firebase.Firebase
 import com.google.firebase.auth.auth
@@ -17,13 +19,11 @@ class MainActivity : Activity() {
 
         // getting current user status
 
-
         // if User is signed in below block will execute (if current user is not null)
         if (Firebase.auth.currentUser?.uid != null) {
 
+            // store UID in application layer
             appUser.UID = Firebase.auth.currentUser?.uid!!
-
-            startActivity(Intent(this,ParkRegisterActivity::class.java))
 
             /*
                 if user is signIn but not filled the registration information
@@ -33,45 +33,51 @@ class MainActivity : Activity() {
             */
 
             // FireStore's method are suspended that why we are using coroutine
-//            CoroutineScope(Dispatchers.Default).launch {
-//
-//                // getting data of user from fireStore
-//
-//                // if user's data has present below block will execute
-//                if (fireStoreUser().userGet(appUser.UID!!) != null) {
-//                    // start the home activity
-//                    startActivity(Intent(
-//                        this@MainActivity,
-//                        HomeActivity::class.java
-//                    ).apply {
-//                        // clear the activity stack
-//                        flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-//                    })
-//                }
-//
-//                // otherwise, user has not registered yet
-//                else {
-//                    // start the registration activity
-//                    startActivity(Intent(
-//                        this@MainActivity,
-//                        UserRegisterActivity::class.java
-//                    ).apply {
-//                        // clear the activity stack
-//                        flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-//                    })
-//                }
-//            }
+            CoroutineScope(Dispatchers.Default).launch {
+
+                // if user's data is present it means user already registered.
+                if (FireStoreUser().userGet(appUser.UID) != null) {
+                    // setting user type as "Finder"
+                    User.type = User.FINDER
+                    nextActivity()
+                }
+
+                // if owner's data is present it means owner's already registered.
+                else if(FireStoreStationLocation().locationGet(appUser.UID) != null){
+                    // setting user type as "Owner"
+                    User.type = User.OWNER
+                    nextActivity()
+                }
+
+                // otherwise, user has not registered yet
+                else{
+                    // start the registration activity
+                    startActivity(Intent(
+                        this@MainActivity,
+                        HowAreYou::class.java
+                    ).apply {
+                        // clear the activity stack
+                        flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                    })
+                }
+            }
         }
 
-        // No user is signed in yet
+        // No user is signed in yet (anonymous)
         else {
-            // through user to the authentication activity
-            startActivity(Intent(
-                this,
-                AuthenticationActivity::class.java
-            ).apply {
-                flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-            })
+            User.type = User.ANONYMOUS
+            // through user to the Home activity
+            startActivity(Intent(this, HomeActivity::class.java))
         }
+    }
+    private fun nextActivity(){
+        // start the home activity
+        startActivity(Intent(
+            this@MainActivity,
+            HomeActivity::class.java
+        ).apply {
+            // clear the activity stack
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        })
     }
 }
