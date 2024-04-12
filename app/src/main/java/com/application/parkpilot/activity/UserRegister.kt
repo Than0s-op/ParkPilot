39894d783189904be14ded3f7801ba8d56efcdc7
Thano0s <himanshupatil45h@gmail.com>
2024-04-12 11:47:21 +0530

    user registration added save waiting, validation, designing

--- app/src/main/java/com/application/parkpilot/activity/UserRegister.kt
+++ app/src/main/java/com/application/parkpilot/activity/UserRegister.kt
@@ -2,9 +2,12 @@ package com.application.parkpilot.activity
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
+import android.widget.ProgressBar
 import android.widget.RadioGroup
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -14,9 +17,12 @@ import coil.load
 import com.application.parkpilot.R
 import com.application.parkpilot.UserCollection
 import com.application.parkpilot.UserProfile
+import com.application.parkpilot.module.PhotoPicker
 import com.application.parkpilot.viewModel.UserRegisterViewModel
+import java.util.Calendar
 
 class UserRegister : AppCompatActivity(R.layout.user_register) {
+    private lateinit var progressBar:ProgressBar
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -29,21 +35,11 @@ class UserRegister : AppCompatActivity(R.layout.user_register) {
         val editTextAge: EditText = findViewById(R.id.editTextAge)
         val radioGroupGender: RadioGroup = findViewById(R.id.radioGroupGender)
         val buttonSave: Button = findViewById(R.id.buttonSave)
-        val editTextPhoneNumber: EditText = findViewById(R.id.editTextPhoneNumber)
-        val editTextEmail: EditText = findViewById(R.id.editTextEmail)
-        val buttonVerifyPhoneNumber: Button = findViewById(R.id.buttonVerifyPhoneNumber)
-        val buttonVerifyEmail: Button = findViewById(R.id.buttonVerifyEmail)
+        progressBar = findViewById(R.id.progressBar)
+        val photoPicker = PhotoPicker(this)
 
         // getting userRegister view model reference
-        val viewModel = ViewModelProvider(this, object : ViewModelProvider.Factory {
-            override fun <T : ViewModel> create(modelClass: Class<T>): T {
-                return UserRegisterViewModel(this@UserRegister) as T
-            }
-        })[UserRegisterViewModel::class.java]
-
-        // it will store MainActivity intent or null
-        // why it's here? ans:- [ if user came from Main Activity then we have to throw user again to Main Activity, otherwise do nothing]
-        var nextIntent: Intent? = Intent(this, Main::class.java)
+        val viewModel = ViewModelProvider(this)[UserRegisterViewModel::class.java]
 
         viewModel.getProfileDetails()
 
@@ -52,28 +48,37 @@ class UserRegister : AppCompatActivity(R.layout.user_register) {
 
         editTextBirthDate.setOnClickListener {
             // start and end dates format should be yyyy-mm-dd (modify this function)
-            viewModel.datePicker.showDatePicker("Select Birth Date", null, null)
+            viewModel.datePicker.showDatePicker(this, "Select Birth Date")
         }
 
         imageViewProfilePicture.setOnClickListener {
             // start photo picker
-            viewModel.photoPicker.showPhotoPicker()
+            photoPicker.showPhotoPicker()
         }
 
         buttonSave.setOnClickListener {
-            // uploading the user data
-            viewModel.saveUserData(
-                this,
-                UserCollection(
-                    editTextFirstName.text.toString(),
-                    editTextLastName.text.toString(),
-                    editTextBirthDate.text.toString(),
-                    if (radioGroupGender.checkedRadioButtonId == R.id.radioButtonFemale) "female" else "male"
-                ),
-                UserProfile(
-                    editTextUserName.text.toString()
+            var isValid = true
+            isValid = isValid(editTextUserName) and isValid
+            isValid = isValid(editTextFirstName) and isValid
+            isValid = isValid(editTextLastName) and isValid
+
+            if (isValid) {
+                showProgress()
+
+                // uploading the user data
+                viewModel.saveUserData(
+                    this,
+                    UserCollection(
+                        editTextFirstName.text.toString(),
+                        editTextLastName.text.toString(),
+                        editTextBirthDate.text.toString(),
+                        if (radioGroupGender.checkedRadioButtonId == R.id.radioButtonFemale) "female" else "male"
+                    ),
+                    UserProfile(
+                        editTextUserName.text.toString()
+                    )
                 )
-            )
+            }
         }
 
         // it is a observer of getImage method's result
@@ -91,8 +96,6 @@ class UserRegister : AppCompatActivity(R.layout.user_register) {
                 editTextBirthDate.setText(it.birthDate)
                 editTextAge.setText(viewModel.getAge(it.birthDate))
                 radioGroupGender.check(if (it.gender == "female") R.id.radioButtonFemale else R.id.radioButtonMale)
-                // if user came from home/other activity except mainActivity
-                nextIntent = null
             }
         }
 
@@ -118,7 +121,7 @@ class UserRegister : AppCompatActivity(R.layout.user_register) {
         }
 
         // it will execute when photo picker get image
-        viewModel.photoPicker.pickedImage.observe(this) { imageUri ->
+        photoPicker.pickedImage.observe(this) { imageUri ->
             // execute below code if imageUri is not null
             imageUri?.let {
                 viewModel.photoUrl = it
@@ -128,20 +131,43 @@ class UserRegister : AppCompatActivity(R.layout.user_register) {
 
         // it will execute when user data uploaded successfully or failed to upload
         viewModel.isUploaded.observe(this) { isUploaded ->
-            if (isUploaded) {
-                Toast.makeText(
-                    this, "Information Save Successfully", Toast.LENGTH_SHORT
-                ).show()
-
-                nextIntent?.let {
-                    startActivity(nextIntent)
-                }
+            unShowProgress()
+            if(isUploaded){
+                Toast.makeText(this,"Details save successfully",Toast.LENGTH_LONG).show()
                 finish()
-            } else {
-                Toast.makeText(
-                    this, "Failed Save Information", Toast.LENGTH_SHORT
-                ).show()
+            }
+            else{
+                Toast.makeText(this,"Failed to save details",Toast.LENGTH_LONG).show()
             }
         }
+    }
+
+    private fun isValid(editText: EditText): Boolean {
+        return if (editText.text.isBlank()) {
+            editText.error = "Must not be blank"
+            false
+        } else {
+            editText.error = null
+            true
+        }
+    }
+
+    private fun showProgress() {
+        // show progress bar
+        progressBar.visibility = View.VISIBLE
+
+        // to disable user interaction with ui
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE,
+            WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE
+        )
+    }
+
+    private fun unShowProgress() {
+        // hide progress bar
+        progressBar.visibility = View.GONE
+
+        // to enable user interaction with ui
+        window.clearFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE)
     }
 }
--- app/src/main/java/com/application/parkpilot/viewModel/UserRegisterViewModel.kt
+++ app/src/main/java/com/application/parkpilot/viewModel/UserRegisterViewModel.kt
@@ -25,9 +25,11 @@ import com.avatarfirst.avatargenlib.AvatarGenerator
 import com.google.firebase.Firebase
 import com.google.firebase.auth.auth
 import kotlinx.coroutines.launch
+import java.text.SimpleDateFormat
 import java.time.LocalDate
+import java.util.Calendar
 
-class UserRegisterViewModel(activity: UserRegister) : ViewModel() {
+class UserRegisterViewModel : ViewModel() {
     // it will store user profile image's Uri
     var photoUrl: Uri? = null
 
@@ -37,17 +39,13 @@ class UserRegisterViewModel(activity: UserRegister) : ViewModel() {
     val imageLoaderResult = MutableLiveData<ImageResult>()
     val isUploaded = MutableLiveData<Boolean>()
 
-    // lazy object creation
-    private val userBasic by lazy { UserBasic() }
-    private val userAdvance by lazy { UserAdvance() }
-    private val storage by lazy { Storage() }
-    val datePicker by lazy { DatePicker(activity) }
-    val photoPicker by lazy { PhotoPicker(activity) }
-
-
-    // it will store MainActivity intent or null
-    // why it's here? ans:- [ if user came from Main Activity then we have to throw user again to Main Activity, otherwise do nothing]
-    var nextIntent: Intent? = Intent(activity, Main::class.java)
+    private val simpleDateFormat = SimpleDateFormat("dd-M-yyyy")
+    private val startDate = simpleDateFormat.parse("25-11-1950")!!
+    private val endDate = simpleDateFormat.parse("25-11-2023")!!
+    private val userBasic = UserBasic()
+    private val userAdvance = UserAdvance()
+    private val storage = Storage()
+    val datePicker = DatePicker(startDate.time,endDate.time)
 
 
     // it will get user detail from user collection
@@ -129,7 +127,7 @@ class UserRegisterViewModel(activity: UserRegister) : ViewModel() {
         }
     }
 
-    fun getAvatar(context: Context, userName: String): Bitmap {
+    private fun getAvatar(context: Context, userName: String): Bitmap {
         return AvatarGenerator.AvatarBuilder(context)
             .setLabel(userName)
             .setAvatarSize(120)
