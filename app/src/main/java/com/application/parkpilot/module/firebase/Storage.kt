01ef2a21095651496a1c74efd8174b8d620af93d
Thano0s <himanshupatil45h@gmail.com>
2024-04-20 20:27:10 +0530

    some changes'

--- app/src/main/java/com/application/parkpilot/module/firebase/Storage.kt
+++ app/src/main/java/com/application/parkpilot/module/firebase/Storage.kt
@@ -3,6 +3,7 @@ package com.application.parkpilot.module.firebase
 import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.drawable.BitmapDrawable
+import android.media.Image
 import android.net.Uri
 import coil.imageLoader
 import coil.request.ImageRequest
@@ -13,20 +14,9 @@ import java.io.ByteArrayOutputStream
 
 class Storage {
     private val storageRef = Firebase.storage.reference
-    suspend fun userProfilePhotoPut(context:Context,uid: String, photo:Any): Uri? {
+    suspend fun userProfilePhotoPut(context: Context, uid: String, image: Any): Uri? {
         val childRef = storageRef.child("user_profile_photo/${uid}")
-        val request = ImageRequest.Builder(context)
-            .data(photo)
-            .size(600, 600)
-            .build()
-        val drawable = context.imageLoader.execute(request).drawable
-
-        // this next leve logic present on firebase doc
-        val bitmap = (drawable as BitmapDrawable).bitmap
-        val baos = ByteArrayOutputStream()
-        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos)
-        val data = baos.toByteArray()
-
+        val data = compress(context,image)
         childRef.putBytes(data).await()
         return userProfilePhotoGet(uid)
     }
@@ -39,41 +29,26 @@ class Storage {
         }
     }
 
-    suspend fun parkSpotPhotoPut(context: Context, uid: String, photosUri: Array<Uri?>):Boolean {
-        val path = "parkSpot/${uid}/"
-        var result = true
-
-        for ((cnt, uri) in photosUri.withIndex()) {
-            val childRef = storageRef.child("$path${cnt}")
-            if(uri == null) {
-                childRef.delete()
-                continue
-            }
-
-            val request = ImageRequest.Builder(context)
-                .data(uri)
-                .size(300, 300)
-                .build()
-            val drawable = context.imageLoader.execute(request).drawable
-
-            // this next leve logic present on firebase doc
-            val bitmap = (drawable as BitmapDrawable).bitmap
-            val baos = ByteArrayOutputStream()
-            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos)
-            val data = baos.toByteArray()
-
-            childRef.putBytes(data).addOnFailureListener{
-                result = false
-            }.await()
-        }
-        return result
-    }
-    suspend fun parkSpotPhotoGet(uid:String):List<Uri>{
+    suspend fun parkSpotPhotoGet(uid: String): List<Uri> {
         val list = storageRef.child("parkSpot/${uid}/").listAll().await()
         val imagesUri = ArrayList<Uri>()
-        for(i in list.items){
+        for (i in list.items) {
             imagesUri.add(i.downloadUrl.await())
         }
         return imagesUri
+    }
+
+    private suspend fun compress(context: Context, image: Any): ByteArray {
+        val request = ImageRequest.Builder(context)
+            .data(image)
+            .size(600, 600)
+            .build()
+        val drawable = context.imageLoader.execute(request).drawable
+
+        // this next level logic present on firebase doc
+        val bitmap = (drawable as BitmapDrawable).bitmap
+        val baos = ByteArrayOutputStream()
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos)
+        return baos.toByteArray()
     }
 }
