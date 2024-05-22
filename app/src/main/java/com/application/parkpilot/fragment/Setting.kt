be16ccf9ca4d3c6885dd1ddc62c0a6c1754eb924
Thano0s <himanshupatil45h@gmail.com>
2024-05-22 18:30:17 +0530

    view binding done in setting

--- app/src/main/java/com/application/parkpilot/fragment/Setting.kt
+++ app/src/main/java/com/application/parkpilot/fragment/Setting.kt
@@ -2,42 +2,34 @@ package com.application.parkpilot.fragment
 
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.application.parkpilot.R
 import com.application.parkpilot.User
+import com.application.parkpilot.databinding.SettingBinding
 import com.application.parkpilot.viewModel.SettingViewModel
 
 class Setting : Fragment(R.layout.setting) {
-    private lateinit var imageViewProfilePicture: ImageView
-    private lateinit var textViewUserName: TextView
-    private lateinit var buttonEditProfile: Button
-    private lateinit var textViewLogin: TextView
-    private lateinit var textViewLogout: TextView
+    private lateinit var binding: SettingBinding
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        imageViewProfilePicture = view.findViewById(R.id.imageViewProfilePicture)
-        textViewUserName = view.findViewById(R.id.textViewUserName)
-        buttonEditProfile = view.findViewById(R.id.buttonEditProfile)
-        textViewLogin = view.findViewById(R.id.textViewLogin)
-        textViewLogout = view.findViewById(R.id.textViewLogout)
 
         loadViews()
 
         val viewModel = SettingViewModel()
 
-        viewModel.loadProfile(requireContext(), imageViewProfilePicture, textViewUserName)
+        viewModel.loadProfile(
+            requireContext(),
+            binding.imageViewProfilePicture,
+            binding.textViewUserName
+        )
 
-        buttonEditProfile.setOnClickListener {
+        binding.buttonEditProfile.setOnClickListener {
             viewModel.personalInformation(requireContext())
         }
-        textViewLogout.setOnClickListener {
+        binding.textViewLogout.setOnClickListener {
             viewModel.logout(requireContext())
         }
-        textViewLogin.setOnClickListener {
+        binding.textViewLogin.setOnClickListener {
             viewModel.login(requireContext())
         }
     }
@@ -45,12 +37,12 @@ class Setting : Fragment(R.layout.setting) {
     private fun loadViews() {
         when (User.type) {
             User.ANONYMOUS -> {
-                buttonEditProfile.visibility = View.GONE
-                textViewLogout.visibility = View.GONE
+                binding.buttonEditProfile.visibility = View.GONE
+                binding.textViewLogout.visibility = View.GONE
             }
 
             User.FINDER -> {
-                textViewLogin.visibility = View.GONE
+                binding.textViewLogin.visibility = View.GONE
             }
         }
     }
