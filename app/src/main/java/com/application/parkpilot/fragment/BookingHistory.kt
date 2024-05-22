9b150a208ea0af865be4e1d24d0e60278680be68
Thano0s <himanshupatil45h@gmail.com>
2024-05-22 18:23:10 +0530

    view binding done in booking history

--- app/src/main/java/com/application/parkpilot/fragment/BookingHistory.kt
+++ app/src/main/java/com/application/parkpilot/fragment/BookingHistory.kt
@@ -5,20 +5,21 @@ import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.application.parkpilot.R
+import com.application.parkpilot.databinding.BookingHistoryBinding
 import com.application.parkpilot.viewModel.BookingHistoryViewModel
 
 class BookingHistory : Fragment(R.layout.booking_history) {
+    private lateinit var binding: BookingHistoryBinding
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val recyclerView = view.findViewById<RecyclerView>(R.id.recycleView).apply {
-            layoutManager = LinearLayoutManager(context)
-        }
+        binding = BookingHistoryBinding.bind(view)
+
+        binding.recyclerView.layoutManager = LinearLayoutManager(context)
 
         // viewModel creation
         val viewModel = ViewModelProvider(this)[BookingHistoryViewModel::class.java]
 
-        viewModel.loadRecycler(requireContext(), recyclerView)
+        viewModel.loadRecycler(requireContext(), binding.recyclerView)
     }
 }
