1c03838ab5745d820af3ce4c6616385cd6a86714
Thano0s <himanshupatil45h@gmail.com>
2024-05-22 18:20:36 +0530

    view binding done in spot preview

--- app/src/main/java/com/application/parkpilot/fragment/bottomSheet/SpotPreview.kt
+++ app/src/main/java/com/application/parkpilot/fragment/bottomSheet/SpotPreview.kt
@@ -5,39 +5,32 @@ import android.content.res.ColorStateList
 import android.graphics.Color
 import android.os.Bundle
 import android.view.View
-import android.widget.TextView
 import androidx.lifecycle.ViewModelProvider
-import androidx.recyclerview.widget.RecyclerView
 import com.application.parkpilot.R
 import com.application.parkpilot.activity.SpotDetail
 import com.application.parkpilot.adapter.recycler.Carousel
+import com.application.parkpilot.databinding.SpotListItemBinding
 import com.application.parkpilot.viewModel.SpotPreviewViewModel
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
-import com.google.android.material.card.MaterialCardView
 import com.google.android.material.carousel.CarouselLayoutManager
 
 class SpotPreview : BottomSheetDialogFragment(R.layout.spot_list_item) {
+    private lateinit var binding: SpotListItemBinding
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val materialCardView: MaterialCardView = view.findViewById(R.id.materialCardView)
-        val recyclerView: RecyclerView = view.findViewById(R.id.recycleView)
-        val textViewName: TextView = view.findViewById(R.id.textViewName)
-        val textViewRating: TextView = view.findViewById(R.id.textViewRating)
-        val textViewNumberOfUser: TextView = view.findViewById(R.id.textViewNumberOfUser)
-        val textViewDistance: TextView = view.findViewById(R.id.textViewDistance)
-        val textViewPrice: TextView = view.findViewById(R.id.textViewPrice)
+        binding = SpotListItemBinding.bind(view)
 
         val stationUID = tag!!
 
         val viewModel = ViewModelProvider(this)[SpotPreviewViewModel::class.java]
 
-        recyclerView.layoutManager = CarouselLayoutManager()
+        binding.recyclerView.layoutManager = CarouselLayoutManager()
         viewModel.loadCarousel(stationUID)
         viewModel.loadBasicInfo(stationUID)
         viewModel.loadRating(stationUID)
 
-        materialCardView.setOnClickListener {
+        binding.materialCardView.setOnClickListener {
             val intent = Intent(context, SpotDetail::class.java).apply {
                 putExtra("stationUID", stationUID)
             }
@@ -45,18 +38,18 @@ class SpotPreview : BottomSheetDialogFragment(R.layout.spot_list_item) {
         }
 
         viewModel.carouselImages.observe(this) { images ->
-            recyclerView.adapter =
+            binding.recyclerView.adapter =
                 Carousel(requireContext(), R.layout.round_carousel, images)
         }
 
         viewModel.stationBasicInfo.observe(this) {
-            textViewName.text = it.name
-            textViewPrice.text = it.price.toString()
+            binding.textViewName.text = it.name
+            binding.textViewPrice.text = it.price.toString()
         }
         viewModel.stationRating.observe(this) {
-            textViewRating.text = String.format("%.1f", it.first / it.second)
-            textViewRating.backgroundTintList = getTint(it.first / it.second)
-            textViewNumberOfUser.text = it.second.toString()
+            binding.textViewRating.text = String.format("%.1f", it.first / it.second)
+            binding.textViewRating.backgroundTintList = getTint(it.first / it.second)
+            binding.textViewNumberOfUser.text = it.second.toString()
         }
     }
 
