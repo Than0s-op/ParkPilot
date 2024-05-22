391c2fe859cc5fd225c03773e5f088bc283837b4
Thano0s <himanshupatil45h@gmail.com>
2024-05-22 10:35:25 +0530

    view binding done in controller

--- app/src/main/java/com/application/parkpilot/activity/Controller.kt
+++ app/src/main/java/com/application/parkpilot/activity/Controller.kt
@@ -1,45 +1,43 @@
 package com.application.parkpilot.activity
 
 import android.os.Bundle
-import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.application.parkpilot.R
 import com.application.parkpilot.User
-import com.application.parkpilot.fragment.BookingHistory
-import com.application.parkpilot.fragment.Map
-import com.application.parkpilot.fragment.Setting
-import com.application.parkpilot.fragment.SpotList
+import com.application.parkpilot.databinding.ControllerBinding
 import com.application.parkpilot.viewModel.Controller
-import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class Controller : AppCompatActivity(R.layout.fragment_controller) {
+class Controller : AppCompatActivity() {
 
+    private lateinit var binding: ControllerBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         // object creation and initialization of views
+        binding = ControllerBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val bottomNavigationView =
-            findViewById<BottomNavigationView>(R.id.bottomNavigationView).apply {
-                itemIconTintList = null
-            }
+        binding.bottomNavigationView.itemIconTintList = null
 
         val viewModel = ViewModelProvider(this)[Controller::class.java]
 
-        setMenuVisibility(bottomNavigationView.menu)
+        setMenuVisibility()
 
 
         // load the profile image in search bar if present
-        viewModel.loadProfileImage(this, bottomNavigationView.menu.findItem(R.id.buttonProfile))
+        viewModel.loadProfileImage(
+            this,
+            binding.bottomNavigationView.menu.findItem(R.id.buttonProfile)
+        )
 
         // default fragment
         changeFragment(viewModel.spotList)
 
         // when search bar menu's items clicked
-        bottomNavigationView.setOnItemSelectedListener { clickedItem ->
-            if (bottomNavigationView.selectedItemId != clickedItem.itemId) {
+        binding.bottomNavigationView.setOnItemSelectedListener { clickedItem ->
+            if (binding.bottomNavigationView.selectedItemId != clickedItem.itemId) {
 
                 when (clickedItem.itemId) {
                     R.id.buttonList -> {
@@ -74,11 +72,11 @@ class Controller : AppCompatActivity(R.layout.fragment_controller) {
         }
     }
 
-    private fun setMenuVisibility(menu: Menu) {
+    private fun setMenuVisibility() {
         User.apply {
             when (type) {
                 ANONYMOUS -> {
-                    menu.findItem(R.id.buttonHistory).isVisible = false
+                    binding.bottomNavigationView.menu.findItem(R.id.buttonHistory).isVisible = false
                 }
 
                 FINDER -> {
