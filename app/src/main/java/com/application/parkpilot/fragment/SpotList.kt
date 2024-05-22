8ab00851f4206793593c3aa14a30c801204364f0
Thano0s <himanshupatil45h@gmail.com>
2024-05-22 18:34:16 +0530

    view binding done in spot list

--- app/src/main/java/com/application/parkpilot/fragment/SpotList.kt
+++ app/src/main/java/com/application/parkpilot/fragment/SpotList.kt
@@ -5,13 +5,11 @@ import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.application.parkpilot.R
 import com.application.parkpilot.adapter.recycler.SpotList
+import com.application.parkpilot.databinding.SpotListBinding
 import com.application.parkpilot.fragment.bottomSheet.SortFragment
 import com.application.parkpilot.viewModel.SpotListViewModel
-import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton
 
 class SpotList : Fragment(R.layout.spot_list),
     SortFragment.OnRadioButtonSelectedListener {
@@ -21,47 +19,43 @@ class SpotList : Fragment(R.layout.spot_list),
     }
 
     private lateinit var viewModel: SpotListViewModel
+    private lateinit var binding: SpotListBinding
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val recyclerView: RecyclerView = view.findViewById(R.id.recyclerView)
-        val swipeRefreshLayout: SwipeRefreshLayout =
-            view.findViewById(R.id.swipeRefreshLayout)
-        val buttonSort: ExtendedFloatingActionButton = view.findViewById(R.id.buttonSort)
-
 
         viewModel = ViewModelProvider(this)[SpotListViewModel::class.java]
         viewModel.loadStation()
-        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+        binding.recyclerView.layoutManager = LinearLayoutManager(requireContext())
         viewModel.permission.gpsPermissionRequest(requireContext())
 
-        buttonSort.setOnClickListener {
+        binding.buttonSort.setOnClickListener {
             val dialog = SortFragment.newInstance(sortingType)
             dialog.onRadioButtonSelectedListener = this
             dialog.show(childFragmentManager, SortFragment.TAG)
         }
 
-        recyclerView.setOnScrollChangeListener { _, _, scrollY, _, oldScrollY ->
+        binding.recyclerView.setOnScrollChangeListener { _, _, scrollY, _, oldScrollY ->
             // the delay of the extension of the FAB is set for 12 items
-            if (scrollY > oldScrollY + 12 && buttonSort.isShown) {
-                buttonSort.hide()
+            if (scrollY > oldScrollY + 12 && binding.buttonSort.isShown) {
+                binding.buttonSort.hide()
             }
 
             // the delay of the extension of the FAB is set for 12 items
-            if (scrollY < oldScrollY - 12 && !buttonSort.isShown) {
-                buttonSort.show()
+            if (scrollY < oldScrollY - 12 && !binding.buttonSort.isShown) {
+                binding.buttonSort.show()
             }
         }
 
-        swipeRefreshLayout.setOnRefreshListener {
+        binding.swipeRefreshLayout.setOnRefreshListener {
             viewModel.loadStation()
             viewModel.permission.gpsPermissionRequest(requireContext())
-            swipeRefreshLayout.isRefreshing = false
+            binding.swipeRefreshLayout.isRefreshing = false
         }
 
         viewModel.liveDataStationLocation.observe(requireActivity()) {
             it?.let {
-                recyclerView.adapter = SpotList(
+                binding.recyclerView.adapter = SpotList(
                     requireContext(), R.layout.spot_list_item, it
                 )
             }
